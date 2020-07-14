42578eb915ca47f890e78132df756a93ab7778f1
qq382724935 <382724935@qq.com>
2020-07-14 10:57:51 +0800

    codepush优化

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,15 @@
  * @Author: 刘利军
  * @Date: 2020-04-21 13:49:03
  * @LastEditors: 刘利军
- * @LastEditTime: 2020-07-10 13:29:19
+ * @LastEditTime: 2020-07-14 09:02:24
  */
 
 import React from 'react';
 import dva from './utils/dva';
 import appModel from './models/app';
 import Router from './router';
-// import codePush from 'react-native-code-push';
+import codePush from 'react-native-code-push';
+let codePushOptions = {checkFrequency: codePush.CheckFrequency.MANUAL};
 
 const app = dva({
   initialState: {},
@@ -20,4 +21,4 @@ const app = dva({
 });
 
 const App = app.start(<Router />);
-export default App;
+export default codePush(codePushOptions)(App);
